5f99e7649ae867378f47f373a3e796c90bf1a093
JamesGrear <jgrear@umich.edu>
2018-04-01 12:22:01 -0400

    Update 2
    - Scene Transition Added
    - Audio added
    - exit prompt added before app quit

---
+++ Zombie Wave Game/Assets/Scripts/MenuTransitions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuTransitions : MonoBehaviour {
+
+    //panel located within main menu that allows player to exit game
+    public GameObject exitpanel;
+
+    public void PlayGameBtn ()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    //This exits the game
+    public void OnExitClick()
+    {
+        exitpanel.SetActive(true);
+    }
+
+    //When Buttons on panel appear....
+    public void NoExit()
+    {
+        exitpanel.SetActive(false); //makes panel dissapear
+    }
+
+    public void YesExit()
+    {
+        Application.Quit();
+    }
+
+}
